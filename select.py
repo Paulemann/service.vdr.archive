03a184c8db74a7b5388a4ecf6671356c728eefce
Paulemann <markra@hotmail.de>
2017-03-08 20:40:19 +0100

    Update select.py

--- select.py
+++ select.py
@@ -15,6 +15,7 @@ from service import get_vdr_reclist, get_vdr_timerlist, is_active_recording, get
 
 
 __addon__ = xbmcaddon.Addon()
+__addon_id__ = __addon__.getAddonInfo('id')
 __addon_path__ = __addon__.getAddonInfo('path')
 __check_icon__ = os.path.join(__addon_path__, 'check.png') # Don't decode _path to utf-8!!!
 __checked_icon__ = os.path.join(__addon_path__, 'checked.png') # Don't decode _path to utf-8!!!
