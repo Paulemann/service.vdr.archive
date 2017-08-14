87f47fc68add043bf3dc8b421baaa1b149c1219a
Paulemann <markra@hotmail.de>
2017-08-14 20:36:34 +0200

    Fixed init error for video genre

--- service.py
+++ service.py
@@ -255,7 +255,7 @@ def get_vdr_recinfo(recdir, extended=False):
     title = subtitle = channel = start = end = ''
     str_subtitle = str_channel = str_start = ''
     estart = length = 0
-    genre = 0
+    genre = ['00']
     episode = 0
     season = 1
 
